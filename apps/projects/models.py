611b368e10d94621f28258beaf596c8e5d1e83ac
Maram <zuaitermaram@gmail.com>
2026-08-21 08:30:43 +0400

    feat: add project models and services models

--- apps/projects/models.py
+++ apps/projects/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from apps.services.models import Service
 
 # Create your models here.
+class ProjectCategory(models.Model):
+    name = models.CharField(max_length=120, unique=True)
+    slug = models.SlugField(max_length=140, unique=True)
+    description = models.TextField(blank=True)
+    is_active = models.BooleanField(default=True)
+    order = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["order", "name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=150, unique=True)
+    slug = models.SlugField(max_length=160, unique=True)
+    description = models.TextField(blank=True, null=True)
+
+    category = models.ForeignKey(ProjectCategory,
+                                on_delete=models.PROTECT, 
+                                related_name='projects')
+    
+    service = models.ForeignKey(Service, 
+                                on_delete=models.PROTECT, 
+                                related_name='projects')
+    
+    image = models.ImageField(upload_to="projects/", blank=True)
+    url = models.URLField(blank=True)
+    is_featured = models.BooleanField(default=False)
+    order = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["order", "created_at"]
+
+    def __str__(self):
+        return self.title
--- apps/services/models.py
+++ apps/services/models.py
@@ -1,3 +1,36 @@
-from django.db import models
+from django.db import models #بستعلم منه انواع الحقول {CharField،TextField،SlugField，BooleanField，ForeignKey，DateTimeField}
 
 # Create your models here.
+
+class Service(models.Model):
+    title = models.CharField(max_length=150, unique=True)
+    slug = models.SlugField(max_length=160, unique=True)
+    description = models.TextField()
+    icon = models.CharField(max_length=100, blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+    order = models.PositiveBigIntegerField(default=0)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+    class Meta:
+        ordering = ['order', 'title']
+
+    def __str__(self):
+        return self.title
+
+
+class ServiceFeature(models.Model):
+    service = models.ForeignKey(Service, on_delete=models.CASCADE, related_name='features')# on_delete=models.CASCADE: means if the service is deleted, all its features will be deleted too
+    title = models.CharField(max_length=150)
+    description = models.TextField(blank=True, null=True)
+    icon = models.CharField(max_length=100, blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+    order = models.PositiveBigIntegerField(default=0)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+    class Meta:
+        ordering = ['order', 'title']
+
+    def __str__(self):
+        return self.title
